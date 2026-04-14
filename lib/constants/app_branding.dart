00a355f28d36cf46eda236523defa6411cd7c832
GT610 <79314033+GT-610@users.noreply.github.com>
2026-04-14 16:37:56 +0800

    Apply Setsuna branding across app shell (#45)

---
+++ lib/constants/app_branding.dart
@@ -0,0 +1,6 @@
+const String kAppName = 'Setsuna';
+const String kAppPackageName = 'aria2_desktop';
+const String kProtocolChannelName = '$kAppPackageName/protocol_integration';
+const String kTrayTooltipSubtitle = 'Aria2 Download Manager';
+const String kAppLogoAssetPath = 'assets/logo/app.png';
+const String kTrayIconAssetPath = 'assets/logo/app.ico';
